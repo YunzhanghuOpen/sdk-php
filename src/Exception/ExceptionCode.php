02b914e423e76fe5a15044e818ab8db466bb1e14
kerun.wu <kerun.wu@yunzhanghu.com>
2023-08-01 18:25:10 +0800

    调整部分注释

--- src/Exception/ExceptionCode.php
+++ src/Exception/ExceptionCode.php
@@ -20,7 +20,7 @@ final class ExceptionCode
 
     private static $error_message_map = array(
         self::CONFIG_ERROR_LOST_KEY => '[客户端] 缺少配置参数',
-        self::CONFIG_ERROR_RSA_KEY_INVALID => '[客户端] RSA 签名算法参数错误',
+        self::CONFIG_ERROR_RSA_KEY_INVALID => '[客户端] RSA 密钥格式错误',
     );
 
     public static function getErrorMessage($code)
