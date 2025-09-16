07d3c8d4021dc0fc15c3e4031d14c1e2325aff1b
bingbing.wang <bingbing.wang@yunzhanghu.com>
2025-09-16 20:44:45 +0800

    订单税费试算接口更新

--- src/Model/Calculatelabor/CalcTaxDetail.php
+++ src/Model/Calculatelabor/CalcTaxDetail.php
@@ -11,23 +11,78 @@ use Yzh\Model\BaseModel;
 class CalcTaxDetail extends BaseModel
 {
     /**
-     * 应纳个税
+     * 预扣个税
      * @var string
      */
     protected $personal_tax;
     /**
-     * 应纳增值税
+     * 预扣增值税
      * @var string
      */
     protected $value_added_tax;
     /**
-     * 应纳附加税费
+     * 预扣附加税费
      * @var string
      */
     protected $additional_tax;
+    /**
+     * 用户预扣个税
+     * @var string
+     */
+    protected $user_personal_tax;
+    /**
+     * 平台企业预扣个税
+     * @var string
+     */
+    protected $dealer_personal_tax;
+    /**
+     * 云账户预扣个税
+     * @var string
+     */
+    protected $broker_personal_tax;
+    /**
+     * 用户预扣增值税
+     * @var string
+     */
+    protected $user_value_added_tax;
+    /**
+     * 平台企业预扣增值税
+     * @var string
+     */
+    protected $dealer_value_added_tax;
+    /**
+     * 云账户预扣增值税
+     * @var string
+     */
+    protected $broker_value_added_tax;
+    /**
+     * 用户预扣附加税费
+     * @var string
+     */
+    protected $user_additional_tax;
+    /**
+     * 平台企业预扣附加税费
+     * @var string
+     */
+    protected $dealer_additional_tax;
+    /**
+     * 云账户预扣附加税费
+     * @var string
+     */
+    protected $broker_additional_tax;
+    /**
+     * 预扣个税税率
+     * @var string
+     */
+    protected $personal_tax_rate;
+    /**
+     * 预扣个税速算扣除数
+     * @var string
+     */
+    protected $deduct_tax;
 
     /**
-     * 应纳个税
+     * 预扣个税
      * @var string $personal_tax
      */
     public function setPersonalTax($personal_tax)
@@ -36,7 +91,7 @@ class CalcTaxDetail extends BaseModel
     }
 
     /**
-     * 应纳个税
+     * 预扣个税
      * @return string
      */
     public function getPersonalTax()
@@ -45,7 +100,7 @@ class CalcTaxDetail extends BaseModel
     }
 
     /**
-     * 应纳增值税
+     * 预扣增值税
      * @var string $value_added_tax
      */
     public function setValueAddedTax($value_added_tax)
@@ -54,7 +109,7 @@ class CalcTaxDetail extends BaseModel
     }
 
     /**
-     * 应纳增值税
+     * 预扣增值税
      * @return string
      */
     public function getValueAddedTax()
@@ -63,7 +118,7 @@ class CalcTaxDetail extends BaseModel
     }
 
     /**
-     * 应纳附加税费
+     * 预扣附加税费
      * @var string $additional_tax
      */
     public function setAdditionalTax($additional_tax)
@@ -72,11 +127,209 @@ class CalcTaxDetail extends BaseModel
     }
 
     /**
-     * 应纳附加税费
+     * 预扣附加税费
      * @return string
      */
     public function getAdditionalTax()
     {
         return $this->additional_tax;
+    }
+
+    /**
+     * 用户预扣个税
+     * @var string $user_personal_tax
+     */
+    public function setUserPersonalTax($user_personal_tax)
+    {
+        $this->user_personal_tax = $user_personal_tax;
+    }
+
+    /**
+     * 用户预扣个税
+     * @return string
+     */
+    public function getUserPersonalTax()
+    {
+        return $this->user_personal_tax;
+    }
+
+    /**
+     * 平台企业预扣个税
+     * @var string $dealer_personal_tax
+     */
+    public function setDealerPersonalTax($dealer_personal_tax)
+    {
+        $this->dealer_personal_tax = $dealer_personal_tax;
+    }
+
+    /**
+     * 平台企业预扣个税
+     * @return string
+     */
+    public function getDealerPersonalTax()
+    {
+        return $this->dealer_personal_tax;
+    }
+
+    /**
+     * 云账户预扣个税
+     * @var string $broker_personal_tax
+     */
+    public function setBrokerPersonalTax($broker_personal_tax)
+    {
+        $this->broker_personal_tax = $broker_personal_tax;
+    }
+
+    /**
+     * 云账户预扣个税
+     * @return string
+     */
+    public function getBrokerPersonalTax()
+    {
+        return $this->broker_personal_tax;
+    }
+
+    /**
+     * 用户预扣增值税
+     * @var string $user_value_added_tax
+     */
+    public function setUserValueAddedTax($user_value_added_tax)
+    {
+        $this->user_value_added_tax = $user_value_added_tax;
+    }
+
+    /**
+     * 用户预扣增值税
+     * @return string
+     */
+    public function getUserValueAddedTax()
+    {
+        return $this->user_value_added_tax;
+    }
+
+    /**
+     * 平台企业预扣增值税
+     * @var string $dealer_value_added_tax
+     */
+    public function setDealerValueAddedTax($dealer_value_added_tax)
+    {
+        $this->dealer_value_added_tax = $dealer_value_added_tax;
+    }
+
+    /**
+     * 平台企业预扣增值税
+     * @return string
+     */
+    public function getDealerValueAddedTax()
+    {
+        return $this->dealer_value_added_tax;
+    }
+
+    /**
+     * 云账户预扣增值税
+     * @var string $broker_value_added_tax
+     */
+    public function setBrokerValueAddedTax($broker_value_added_tax)
+    {
+        $this->broker_value_added_tax = $broker_value_added_tax;
+    }
+
+    /**
+     * 云账户预扣增值税
+     * @return string
+     */
+    public function getBrokerValueAddedTax()
+    {
+        return $this->broker_value_added_tax;
+    }
+
+    /**
+     * 用户预扣附加税费
+     * @var string $user_additional_tax
+     */
+    public function setUserAdditionalTax($user_additional_tax)
+    {
+        $this->user_additional_tax = $user_additional_tax;
+    }
+
+    /**
+     * 用户预扣附加税费
+     * @return string
+     */
+    public function getUserAdditionalTax()
+    {
+        return $this->user_additional_tax;
+    }
+
+    /**
+     * 平台企业预扣附加税费
+     * @var string $dealer_additional_tax
+     */
+    public function setDealerAdditionalTax($dealer_additional_tax)
+    {
+        $this->dealer_additional_tax = $dealer_additional_tax;
+    }
+
+    /**
+     * 平台企业预扣附加税费
+     * @return string
+     */
+    public function getDealerAdditionalTax()
+    {
+        return $this->dealer_additional_tax;
+    }
+
+    /**
+     * 云账户预扣附加税费
+     * @var string $broker_additional_tax
+     */
+    public function setBrokerAdditionalTax($broker_additional_tax)
+    {
+        $this->broker_additional_tax = $broker_additional_tax;
+    }
+
+    /**
+     * 云账户预扣附加税费
+     * @return string
+     */
+    public function getBrokerAdditionalTax()
+    {
+        return $this->broker_additional_tax;
+    }
+
+    /**
+     * 预扣个税税率
+     * @var string $personal_tax_rate
+     */
+    public function setPersonalTaxRate($personal_tax_rate)
+    {
+        $this->personal_tax_rate = $personal_tax_rate;
+    }
+
+    /**
+     * 预扣个税税率
+     * @return string
+     */
+    public function getPersonalTaxRate()
+    {
+        return $this->personal_tax_rate;
+    }
+
+    /**
+     * 预扣个税速算扣除数
+     * @var string $deduct_tax
+     */
+    public function setDeductTax($deduct_tax)
+    {
+        $this->deduct_tax = $deduct_tax;
+    }
+
+    /**
+     * 预扣个税速算扣除数
+     * @return string
+     */
+    public function getDeductTax()
+    {
+        return $this->deduct_tax;
     }
 }
--- src/Model/Calculatelabor/CalcTaxResponseData.php
+++ src/Model/Calculatelabor/CalcTaxResponseData.php
@@ -12,7 +12,7 @@ use Yzh\Model\ResponseDataInterface;
 class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
 {
     /**
-     * 订单金额
+     * 测算金额
      * @var string
      */
     protected $pay;
@@ -22,18 +22,63 @@ class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
      */
     protected $tax;
     /**
-     * 税后金额
+     * 税后结算金额
      * @var string
      */
     protected $after_tax_amount;
     /**
-     * 税费明细
+     * 缴税明细
      * @var CalcTaxDetail
      */
     protected $tax_detail;
+    /**
+     * 税前订单金额
+     * @var string
+     */
+    protected $before_tax_amount;
+    /**
+     * 用户税费总额
+     * @var string
+     */
+    protected $user_tax;
+    /**
+     * 平台企业税费总额
+     * @var string
+     */
+    protected $dealer_tax;
+    /**
+     * 云账户税费总额
+     * @var string
+     */
+    protected $broker_tax;
+    /**
+     * 用户服务费
+     * @var string
+     */
+    protected $user_fee;
+    /**
+     * 结果
+     * @var string
+     */
+    protected $status;
+    /**
+     * 结果详细状态码
+     * @var string
+     */
+    protected $status_detail;
+    /**
+     * 结果说明
+     * @var string
+     */
+    protected $status_message;
+    /**
+     * 结果详细状态码描述
+     * @var string
+     */
+    protected $status_detail_message;
 
     /**
-     * 订单金额
+     * 测算金额
      * @var string $pay
      */
     public function setPay($pay)
@@ -42,7 +87,7 @@ class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
     }
 
     /**
-     * 订单金额
+     * 测算金额
      * @return string
      */
     public function getPay()
@@ -69,7 +114,7 @@ class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
     }
 
     /**
-     * 税后金额
+     * 税后结算金额
      * @var string $after_tax_amount
      */
     public function setAfterTaxAmount($after_tax_amount)
@@ -78,7 +123,7 @@ class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
     }
 
     /**
-     * 税后金额
+     * 税后结算金额
      * @return string
      */
     public function getAfterTaxAmount()
@@ -87,7 +132,7 @@ class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
     }
 
     /**
-     * 税费明细
+     * 缴税明细
      * @var CalcTaxDetail $tax_detail
      */
     public function setTaxDetail($tax_detail)
@@ -96,11 +141,173 @@ class CalcTaxResponseData extends BaseModel implements ResponseDataInterface
     }
 
     /**
-     * 税费明细
+     * 缴税明细
      * @return CalcTaxDetail
      */
     public function getTaxDetail()
     {
         return $this->tax_detail;
+    }
+
+    /**
+     * 税前订单金额
+     * @var string $before_tax_amount
+     */
+    public function setBeforeTaxAmount($before_tax_amount)
+    {
+        $this->before_tax_amount = $before_tax_amount;
+    }
+
+    /**
+     * 税前订单金额
+     * @return string
+     */
+    public function getBeforeTaxAmount()
+    {
+        return $this->before_tax_amount;
+    }
+
+    /**
+     * 用户税费总额
+     * @var string $user_tax
+     */
+    public function setUserTax($user_tax)
+    {
+        $this->user_tax = $user_tax;
+    }
+
+    /**
+     * 用户税费总额
+     * @return string
+     */
+    public function getUserTax()
+    {
+        return $this->user_tax;
+    }
+
+    /**
+     * 平台企业税费总额
+     * @var string $dealer_tax
+     */
+    public function setDealerTax($dealer_tax)
+    {
+        $this->dealer_tax = $dealer_tax;
+    }
+
+    /**
+     * 平台企业税费总额
+     * @return string
+     */
+    public function getDealerTax()
+    {
+        return $this->dealer_tax;
+    }
+
+    /**
+     * 云账户税费总额
+     * @var string $broker_tax
+     */
+    public function setBrokerTax($broker_tax)
+    {
+        $this->broker_tax = $broker_tax;
+    }
+
+    /**
+     * 云账户税费总额
+     * @return string
+     */
+    public function getBrokerTax()
+    {
+        return $this->broker_tax;
+    }
+
+    /**
+     * 用户服务费
+     * @var string $user_fee
+     */
+    public function setUserFee($user_fee)
+    {
+        $this->user_fee = $user_fee;
+    }
+
+    /**
+     * 用户服务费
+     * @return string
+     */
+    public function getUserFee()
+    {
+        return $this->user_fee;
+    }
+
+    /**
+     * 结果
+     * @var string $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * 结果
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * 结果详细状态码
+     * @var string $status_detail
+     */
+    public function setStatusDetail($status_detail)
+    {
+        $this->status_detail = $status_detail;
+    }
+
+    /**
+     * 结果详细状态码
+     * @return string
+     */
+    public function getStatusDetail()
+    {
+        return $this->status_detail;
+    }
+
+    /**
+     * 结果说明
+     * @var string $status_message
+     */
+    public function setStatusMessage($status_message)
+    {
+        $this->status_message = $status_message;
+    }
+
+    /**
+     * 结果说明
+     * @return string
+     */
+    public function getStatusMessage()
+    {
+        return $this->status_message;
+    }
+
+    /**
+     * 结果详细状态码描述
+     * @var string $status_detail_message
+     */
+    public function setStatusDetailMessage($status_detail_message)
+    {
+        $this->status_detail_message = $status_detail_message;
+    }
+
+    /**
+     * 结果详细状态码描述
+     * @return string
+     */
+    public function getStatusDetailMessage()
+    {
+        return $this->status_detail_message;
     }
 }
